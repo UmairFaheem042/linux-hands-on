89a6a14699abe8af2e9e218428d2b471c26ed5f4
Umair Faheem <umair.freelancer042@gmail.com>
2026-03-08 19:51:57 +0530

    Started with log rotation

--- 03-log-rotation-and-archive/bin/script.sh
+++ 03-log-rotation-and-archive/bin/script.sh
@@ -2,8 +2,30 @@
 
 set -euo pipefail
 
-main(){
-    
+# find logs
+find_old_logs(){
+    # find logs (files) in a directory (/var/log/myApp) -- no subdirectory
+    find -type f /var/log/myApp -name "*.log"
+
+    # find logs older than 7 days => use mtime option
+    find -type f /var/log/myApp -name "*.log" -mtime +7
 }
+
+# convert founded logs to .tar
+convert_to_tar(){
+    # tar [flags] destinationFileName sourceFileName
+}
+
+# convert .tar to .gz
+conver_to_gz(){}
+
+# convert .tar then .gz in one command
+conver_to_tar_gz(){}
+
+# verify files created and delete the original
+verify_and_delete(){}
+
+# main entry point
+main(){}
 
 main
